a3f16e382c660ba8bbf630113a808ec5f76f5d77
Alvarojoered <arojaspen@gmail.com>
2017-05-23 11:14:57 -0700

    Create login form route and mixin view

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const planController = require('../controllers/planController')
-
+const userController = require('../controllers/userController')
 
 router.get('/', planController.landingPage)
+
+// user routes
+router.get('/login', userController.loginForm)
 
 module.exports = router;
