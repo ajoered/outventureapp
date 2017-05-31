73b0737166a5ca7a3655106f3fe7329ff6e0ccc1
Alvarojoered <arojaspen@gmail.com>
2017-05-31 16:44:19 -0700

    Update new style JS, CSS and Pug files

--- public/dist/App.bundle.js
+++ public/dist/App.bundle.js
@@ -63,7 +63,7 @@
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 2);
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
 /******/ })
 /************************************************************************/
 /******/ ([
@@ -73,45 +73,126 @@
 "use strict";
 
 
-Object.defineProperty(exports, "__esModule", {
-  value: true
+$(document).ready(function () {
+    $('.button-collapse').sideNav();
+
+    scaleVideoContainer();
+
+    initBannerVideoSize('.video-container .poster img');
+    initBannerVideoSize('.video-container .filter');
+    initBannerVideoSize('.video-container video');
+
+    $(window).on('resize', function () {
+        scaleVideoContainer();
+        scaleBannerVideoSize('.video-container .poster img');
+        scaleBannerVideoSize('.video-container .filter');
+        scaleBannerVideoSize('.video-container video');
+    });
+
+    initMap();
+    checkScroll();
+    scrollMagic();
 });
-// based on https://gist.github.com/paulirish/12fb951a8b893a454b32
 
-var $ = document.querySelector.bind(document);
-var $$ = document.querySelectorAll.bind(document);
+function checkScroll() {
+    var startY = $('.nav-wrapper').height() * 1; //The point where the nav-wrapper changes in px
 
-Node.prototype.on = window.on = function (name, fn) {
-  this.addEventListener(name, fn);
-};
+    if ($(window).scrollTop() > startY) {
+        $('.nav-wrapper').addClass("scrolled");
+        document.getElementById("nav-logo").src = "https://res.cloudinary.com/dx1s7kdgz/image/upload/v1495057462/compundBlack_mlkuff.png";
+    } else {
+        $('.nav-wrapper').removeClass("scrolled");
+        document.getElementById("nav-logo").src = "https://res.cloudinary.com/dx1s7kdgz/image/upload/v1494986580/outventure_white-01_gofmj3.png";
+    }
+}
 
-NodeList.prototype.__proto__ = Array.prototype; // eslint-disable-line
+if ($('.nav-wrapper').length > 0) {
+    $(window).on("scroll load resize", function () {
+        checkScroll();
+    });
+}
 
-NodeList.prototype.on = NodeList.prototype.addEventListener = function (name, fn) {
-  this.forEach(function (elem) {
-    elem.on(name, fn);
-  });
-};
+function scaleVideoContainer() {
 
-exports.$ = $;
-exports.$$ = $$;
+    var height = $(window).height() + 5;
+    var unitHeight = parseInt(height) + 'px';
+    $('.homepage-hero-module').css('height', unitHeight);
+}
 
-/***/ }),
-/* 1 */
-/***/ (function(module, exports) {
+function initBannerVideoSize(element) {
 
-// removed by extract-text-webpack-plugin
+    $(element).each(function () {
+        $(this).data('height', $(this).height());
+        $(this).data('width', $(this).width());
+    });
 
-/***/ }),
-/* 2 */
-/***/ (function(module, exports, __webpack_require__) {
+    scaleBannerVideoSize(element);
+}
 
-"use strict";
+function scaleBannerVideoSize(element) {
 
+    var windowWidth = $(window).width(),
+        windowHeight = $(window).height() + 5,
+        videoWidth,
+        videoHeight;
 
-__webpack_require__(1);
+    // console.log(windowHeight);
 
-var _bling = __webpack_require__(0);
+    $(element).each(function () {
+        var videoAspectRatio = $(this).data('height') / $(this).data('width');
+
+        $(this).width(windowWidth);
+
+        if (windowWidth < 1000) {
+            videoHeight = windowHeight;
+            videoWidth = videoHeight / videoAspectRatio;
+            $(this).css({ 'margin-top': 0, 'margin-left': -(videoWidth - windowWidth) / 2 + 'px' });
+
+            $(this).width(videoWidth).height(videoHeight);
+        }
+
+        $('.homepage-hero-module .video-container video').addClass('fadeIn animated');
+    });
+}
+
+var text = ["happy", "healthy", "mindful", "fit"];
+var counter = 0;
+var elem = document.getElementById("text-change");
+setInterval(change, 1500);
+function change() {
+    elem.innerHTML = text[counter];
+    counter++;
+    if (counter >= text.length) {
+        counter = 0;
+    }
+}
+
+function initMap() {
+    var uluru = { lat: -25.363, lng: 131.044 };
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 4,
+        center: uluru
+    });
+    var marker = new google.maps.Marker({
+        position: uluru,
+        map: map
+    });
+}
+
+function scrollMagic() {
+    var controller = new ScrollMagic.Controller();
+
+    var sceneSearchNav = new ScrollMagic.Scene({ triggerElement: "#search-nav" }).setPin("#search-nav").addTo(controller);
+
+    var offset = sceneSearchNav.offset();
+    sceneSearchNav.offset(286);
+
+    var mapFix = new ScrollMagic.Scene({
+        triggerElement: ".listings-wrapper",
+        triggerHook: "onLeave",
+        duration: 0
+    }).setClassToggle(".map-container", "fixed").addTo(controller);
+}
 
 /***/ })
 /******/ ]);
--- public/javascripts/init.js
+++ public/javascripts/init.js
@@ -1,140 +1,125 @@
-(function($){
-	$(function(){
+$(document).ready(function(){
+  $('.button-collapse').sideNav();
 
-		function showCards() {
-			var html = `
-			<div class="col l4 m6 s12 gallery-item gallery-expand gallery-filter adventure">
-				<div class="gallery-curve-wrapper">
-					<a class="gallery-cover gray" style="height: 250px;">
-						<img src="images/surf.jpg" style="height: 100%; width: auto;" alt="placeholder">
-					</a>
-					<div class="gallery-header">
-						<span>Surf </span>
+  scaleVideoContainer();
 
-					</div>
-					<div class="gallery-body">
-						<div class="title-wrapper">
-							<h3>Surf</h3>
-						</div>
-						<p class="description">
-							Literally venmo before they sold out, DIY heirloom forage polaroid offal yr pop-up selfies health goth. Typewriter scenester hammock truffaut meditation, squid before they sold out polaroid portland tousled taxidermy vice. Listicle butcher thundercats, taxidermy pitchfork next level roof party crucifix narwhal kinfolk you probably haven't heard of them portland small batch.</p>
-							<p class="description">
-								<hr>
-								Ea salvia adipisicing vegan man bun. Flexitarian cupidatat skateboard flannel. Drinking vinegar marfa you probably haven't heard of them consequat post-ironic, shabby chic williamsburg raclette vaporware readymade selfies brunch. Venmo selvage biodiesel marfa. Tbh literally 3 wolf moon, proident elit raclette chambray consequat edison bulb four loko accusamus. Semiotics godard eiusmod, ex esse air plant quinoa vaporware selfies keytar. Actually yuccie ennui flannel single-origin coffee, williamsburg cardigan banjo forage pug distillery tumblr hexagon vinyl occaecat.</p>
-							</div>
-							<div class="gallery-action">
-								<a class="btn-floating btn-large waves-effect waves-light"><i class="material-icons">favorite</i></a>
-								<a class="btn-floating btn-large waves-effect waves-light"><i class="material-icons">add</i></a>
-							</div>
-						</div>
-					</div>
-			`
-			var div = document.createElement('div');
-			div.innerHTML = html;
+  initBannerVideoSize('.video-container .poster img');
+  initBannerVideoSize('.video-container .filter');
+  initBannerVideoSize('.video-container video');
 
-			document.getElementById('results').appendChild(div);
-  	};
+  $(window).on('resize', function() {
+      scaleVideoContainer();
+      scaleBannerVideoSize('.video-container .poster img');
+      scaleBannerVideoSize('.video-container .filter');
+      scaleBannerVideoSize('.video-container video');
+  });
 
-    // Navbar
-    $(".button-collapse").sideNav();
-    var categories = $('nav .categories-container');
-    if (categories.length) {
-      categories.pushpin({ top: categories.offset().top });
-      var $links = categories.find('li');
-      $links.each(function() {
-        var $link = $(this);
-        $link.on('click', function() {
-          $links.removeClass('active');
-          $link.addClass('active');
-          var hash = $link.find('a').first()[0].hash.substr(1);
-          var $galleryItems = $('.gallery .gallery-item');
+  initMap();
+  checkScroll();
+  scrollMagic();
+});
 
-          $galleryItems.stop().addClass('gallery-filter').fadeIn(100);
+function checkScroll(){
+    var startY = $('.nav-wrapper').height() * 1; //The point where the nav-wrapper changes in px
 
-          if (hash !== 'all') {
-            var $galleryFilteredOut = $galleryItems.not('.' + hash).not('.all');
-            $galleryFilteredOut.removeClass('gallery-filter').hide();
-          }
-
-          // transition layout
-          $masonry.masonry({
-            transitionDuration: '.3s'
-          });
-          // only animate on layout
-          $masonry.one( 'layoutComplete', function( event, items ) {
-            $masonry.masonry({
-              transitionDuration: 0
-            });
-          });
-          setTimeout(function() {
-
-          $masonry.masonry('layout');
-          }, 1000);
-        });
-      });
+    if($(window).scrollTop() > startY){
+        $('.nav-wrapper').addClass("scrolled");
+        document.getElementById("nav-logo").src="https://res.cloudinary.com/dx1s7kdgz/image/upload/v1495057462/compundBlack_mlkuff.png";
+    }else{
+        $('.nav-wrapper').removeClass("scrolled");
+        document.getElementById("nav-logo").src="https://res.cloudinary.com/dx1s7kdgz/image/upload/v1494986580/outventure_white-01_gofmj3.png";
     }
+}
 
+if($('.nav-wrapper').length > 0){
+    $(window).on("scroll load resize", function(){
+        checkScroll();
+    });
+}
 
-	  // Home
-	  $('.carousel:not(.carousel-slider)').carousel({
-      dist: 0,
-      padding: 10
-    });
-    $('.carousel.carousel-slider').carousel({
-      fullWidth: true,
-      indicators: true,
-      onCycleTo: function(el) {
-        $('.nav-background img').removeClass('active');
-        $('.nav-background img').eq(el.index()).addClass('active');
-      }
-    });
+function scaleVideoContainer() {
 
+    var height = $(window).height() + 5;
+    var unitHeight = parseInt(height) + 'px';
+    $('.homepage-hero-module').css('height',unitHeight);
 
-    // Masonry Grid
-    var $masonry = $('.gallery');
-    $masonry.masonry({
-      // set itemSelector so .grid-sizer is not used in layout
-      itemSelector: '.gallery-filter',
-      // use element for option
-      columnWidth: '.gallery-filter',
-      // no transitions
-      transitionDuration: 0
-    });
-    // layout Masonry after each image loads
-    $masonry.imagesLoaded(function() {
-      $masonry.masonry('layout');
-    });
-    $('a.filter').click(function (e) {
-      e.preventDefault();
+}
+
+function initBannerVideoSize(element){
+
+    $(element).each(function(){
+        $(this).data('height', $(this).height());
+        $(this).data('width', $(this).width());
     });
 
+    scaleBannerVideoSize(element);
 
+}
 
-    // Contact Form Icon
-    $("form .form-control").focus(function() {
-      $(this).siblings("label").first().children("i").first().css({"color": "#aaa", "left": 0});
+function scaleBannerVideoSize(element){
+
+    var windowWidth = $(window).width(),
+    windowHeight = $(window).height() + 5,
+    videoWidth,
+    videoHeight;
+
+    // console.log(windowHeight);
+
+    $(element).each(function(){
+        var videoAspectRatio = $(this).data('height')/$(this).data('width');
+
+        $(this).width(windowWidth);
+
+        if(windowWidth < 1000){
+            videoHeight = windowHeight;
+            videoWidth = videoHeight / videoAspectRatio;
+            $(this).css({'margin-top' : 0, 'margin-left' : -(videoWidth - windowWidth) / 2 + 'px'});
+
+            $(this).width(videoWidth).height(videoHeight);
+        }
+
+        $('.homepage-hero-module .video-container video').addClass('fadeIn animated');
+
     });
-    $("form .form-control").blur(function() {
-      $(this).siblings("label").first().children("i").first().css({"color": "transparent", "left": "-20px"});
-    });
+}
+
+var text = ["happy", "healthy", "mindful", "fit"];
+var counter = 0;
+var elem = document.getElementById("text-change");
+setInterval(change, 1500);
+function change() {
+ elem.innerHTML = text[counter];
+    counter++;
+    if(counter >= text.length) { counter = 0; }
+}
+
+function initMap() {
+  var uluru = {lat: -25.363, lng: 131.044};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 4,
+    center: uluru
+  });
+  var marker = new google.maps.Marker({
+    position: uluru,
+    map: map
+  });
+}
+
+function scrollMagic() {
+  var controller = new ScrollMagic.Controller();
+
+  var sceneSearchNav = new ScrollMagic.Scene({triggerElement: "#search-nav"})
+        .setPin("#search-nav")
+        .addTo(controller);
+
+  var offset = sceneSearchNav.offset();
+  sceneSearchNav.offset(286);
+
+  var mapFix =  new ScrollMagic.Scene({
+      triggerElement: ".listings-wrapper",
+      triggerHook: "onLeave",
+      duration: 0
+  }).setClassToggle(".map-container", "fixed").addTo(controller);
 
 
-    var onShow = function(el) {
-      var carousel = el.find('.carousel.initialized');
-      carousel.carousel({
-        dist: 0,
-        padding: 10
-      });
-    };
-    $('.gallery-expand').galleryExpand({
-      onShow: onShow
-    });
-
-    $('.blog .gallery-expand').galleryExpand({
-      onShow: onShow,
-      fillScreen: true,
-      inDuration: 500,
-    });
-
-	}); // end of document ready
-})(jQuery); // end of jQuery name space
+}
