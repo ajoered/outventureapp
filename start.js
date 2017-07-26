6b033f17ffee4592c21150e4bdf0d34bcb642f2b
Alvarojoered <arojaspen@gmail.com>
2017-07-25 23:49:42 -0700

    change model path to root at start.js

--- start.js
+++ start.js
@@ -11,9 +11,9 @@ mongoose.connection.on('error', (err) => {
 });
 
 //Import models
-require('./models/plan.js')
-require('./models/user.js')
-require('./models/review.js')
+require('../models/plan.js')
+require('../models/user.js')
+require('../models/review.js')
 
 // Start the app
 const app = require('./app');
